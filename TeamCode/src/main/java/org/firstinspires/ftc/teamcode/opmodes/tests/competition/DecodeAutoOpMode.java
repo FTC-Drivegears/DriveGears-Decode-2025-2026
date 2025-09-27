8ee168aeaf52b7cf8f61f5ea9f70cbbe0751518e
shark <ck.barenthin@gmail.com>
2025-09-27 14:36:28 -0400

    added code for colour sensor to detect between purple ball and green ball, and when no ball is detected.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/competition/DecodeAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/competition/DecodeAutoOpMode.java
@@ -1,4 +1,50 @@
 package org.firstinspires.ftc.teamcode.opmodes.tests.competition;
 
-public class DecodeAutoOpMode {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "colourSensor")
+
+public class DecodeAutoOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        ColorSensor colourSensor;
+        colourSensor = hardwareMap.get(ColorSensor.class, "colour");
+        Servo servo = hardwareMap.get(Servo.class, "servo");
+        colourSensor.enableLed(true);
+
+        servo.setPosition(0);
+
+       waitForStart();
+
+        while (opModeIsActive()) {
+            int red = colourSensor.red();
+            int green = colourSensor.green();
+            int blue = colourSensor.blue();
+            int alpha = colourSensor.alpha();
+
+            telemetry.addData("Red", red);
+            telemetry.addData("Green", green);
+            telemetry.addData("Blue", blue);
+            telemetry.addData("Alpha", alpha);
+            telemetry.update();
+
+            // Purple ball
+            if(blue > green) {
+                // Purple ball is detected
+                servo.setPosition(1);
+            }
+            // Green ball
+            else if(green - blue > 30) {
+                // Green ball is detected
+                servo.setPosition(0);
+            }
+            else {
+                // No ball
+                servo.setPosition(0.5);
+            }
+        }
+    }
 }
