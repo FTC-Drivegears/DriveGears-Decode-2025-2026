104914bcc3ba2080c64ec1c4c094d293a82e28b6
Kayla0129 <kaylatongzhao@gmail.com>
2025-10-11 17:39:23 -0400

    Autochanges

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SampleAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SampleAutoOpMode.java
@@ -1,9 +1,4 @@
 package org.firstinspires.ftc.teamcode.opmodes.tests;
-<<<<<<< HEAD
-
-import com.acmerobotics.dashboard.FtcDashboard;
-=======
->>>>>>> 64897af (Auto)
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -12,7 +7,8 @@ import org.firstinspires.ftc.teamcode.Hardware;
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
 
 
-@Autonomous (name = "SampleAutoOpMode")
+
+@Autonomous (name = "Sample Auto")
 public class SampleAutoOpMode extends LinearOpMode {
     private MecanumCommand mecanumCommand;
     private int stage1 = 0;
@@ -28,28 +24,6 @@ public class SampleAutoOpMode extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         // create Hardware using hardwareMap
         Hardware hw = Hardware.getInstance(hardwareMap);
-<<<<<<< HEAD
-        mecanumCommand = new MecanumCommand(hw, telemetry);
-
-        boolean firstInstance = true;
-
-        ElapsedTime timer;
-        AUTO_STATE autoState = AUTO_STATE.FIRST_BUCKET;
-        waitForStart();
-        while (opModeIsActive()) {
-            // run processes
-            //updateTelemetry();
-            mecanumCommand.motorProcess();
-            mecanumCommand.processOdometry();
-
-
-            switch (autoState) {
-                case FIRST_BUCKET:
-                    telemetry.addLine("running");
-                    if (mecanumCommand.moveToPos(0, 50, 0.0)) {
-
-                        autoState = AUTO_STATE.FINISH;
-=======
         mecanumCommand = new MecanumCommand(hw);
 
         AUTO_STATE autoState = AUTO_STATE.FIRST_BUCKET;
@@ -64,7 +38,6 @@ public class SampleAutoOpMode extends LinearOpMode {
                     mecanumCommand.moveToPos(30, 0, 0);
                     if (mecanumCommand.positionNotReachedYet()) {
                         autoState = AUTO_STATE.SUB_PICKUP;
->>>>>>> 64897af (Auto)
                     }
                     break;
                 case SUB_PICKUP:
@@ -74,25 +47,13 @@ public class SampleAutoOpMode extends LinearOpMode {
                     break;
                 case FINISH:
                     stopRobot();
-                    telemetry.addLine("finished");
                     break;
             }
         }
+
     }
-<<<<<<< HEAD
-    public void updateTelemetry() {
-        packet.put("x: ", mecanumCommand.getOdoX());
-        packet.put("y: ", mecanumCommand.getOdoY());
-        packet.put("theta: ", mecanumCommand.getOdoHeading());
-    }
-=======
->>>>>>> 64897af (Auto)
 
     private void stopRobot() {
         mecanumCommand.moveGlobalPartialPinPoint(0, 0, 0);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 64897af (Auto)
