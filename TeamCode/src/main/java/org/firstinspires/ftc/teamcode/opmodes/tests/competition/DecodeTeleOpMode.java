7416dfbb5488b3454c54f291eaef3f5359913030
OrangeJuice76 <alenacao888@gmail.com>
2025-10-14 19:05:28 -0400

    feat(intake): no need to hold the "A" button for intake to spin. Also cleaned up the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/competition/DecodeTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/competition/DecodeTeleOpMode.java
@@ -3,9 +3,6 @@ package org.firstinspires.ftc.teamcode.opmodes.tests.competition;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.teamcode.Hardware;
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
 
@@ -14,18 +11,8 @@ import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
 public class DecodeTeleOpMode extends LinearOpMode {
     private MecanumCommand mecanumCommand;
 
-    private static Hardware instance;
-
     private Hardware hw;
     private double theta;
-
-    //private DcMotor motorTurret;
-//    private DcMotor motorHood;
-
-    private Servo servoSorter;
-    private Servo servoPusher;
-
-    private ElapsedTime resetTimer;
     private DcMotor intakeMotor;
 
     @Override
@@ -33,68 +20,17 @@ public class DecodeTeleOpMode extends LinearOpMode {
 
         this.intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
 
-        this.servoSorter = hardwareMap.get(Servo.class, "sorter");
-        this.servoPusher = hardwareMap.get(Servo.class, "pusher");
-
         boolean isIntakeMotorOn = false;
         hw = Hardware.getInstance(hardwareMap);
         mecanumCommand = new MecanumCommand(hw);
-        resetTimer = new ElapsedTime();
-
-        double setServoSorterPosition = 0;
-        double supposedServoSorterPosition = 0;
-
-        servoSorter.setPosition(setServoSorterPosition);
 
         while (opModeInInit()){
             telemetry.update();
         }
-
         // Wait for start button to be pressed
         waitForStart();
 
-        // Loop while OpMode is running
-
         while (opModeIsActive()) {
-            if (gamepad1.right_trigger > 0) {
-                servoPusher.setPosition(1);
-                // Launch for as long as the button is pressed
-                //motorTurret.setPower(gamepad1.right_trigger);
-            }
-//            if (gamepad1.left_bumper) {
-//                motorHood.setPower(-gamepad1.left_trigger);
-//            }
-//            else {
-//                motorHood.setPower(gamepad1.left_trigger);
-//            }
-
-            // Needs correction
-            servoPusher.setPosition(0);
-            //Thread.sleep(1000);
-            if (gamepad1.x) {
-                // Go to next sort
-                if (servoSorter.getPosition() == 0.3+setServoSorterPosition) {
-                    supposedServoSorterPosition = 0.6;
-                } else if (servoSorter.getPosition() == 0.6+setServoSorterPosition) {
-                    supposedServoSorterPosition = 1;
-                } else {
-                    supposedServoSorterPosition = 0.3;
-                }
-            }
-
-            if (gamepad1.y) {
-                // Manual override
-                setServoSorterPosition += 0.05;
-            }
-
-            setServoSorterPosition = (setServoSorterPosition+supposedServoSorterPosition)%1;
-            servoSorter.setPosition(setServoSorterPosition);
-            // No longer needs correction
-
-            // Intake
-            if (gamepad1.right_bumper) {
-                //motorTurret.setPower(0.4);
-            }
 
             theta = mecanumCommand.fieldOrientedMove(
                     gamepad1.left_stick_y,
@@ -102,25 +38,28 @@ public class DecodeTeleOpMode extends LinearOpMode {
                     gamepad1.right_stick_x
             );
 
+            if(gamepad1.a && isIntakeMotorOn){
+                isIntakeMotorOn = false;
+            } else if (gamepad1.a){
+                isIntakeMotorOn = true;
+            }
 
-            isIntakeMotorOn = gamepad1.a;
-            telemetry.addData("Is intake motor ON?: ", isIntakeMotorOn);
-            telemetry.update();
             if (isIntakeMotorOn){
-                intakeMotor.setPower(-0.5);
+                intakeMotor.setPower(-0.4);
             } else{
                 intakeMotor.setPower(0);
             }
 
-            //motorTurret.setPower(0);
+            telemetry.addData("Is intake motor ON?: ", isIntakeMotorOn);
+            telemetry.update();
+//            isIntakeMotorOn = gamepad1.a;
+//            telemetry.addData("Is intake motor ON?: ", isIntakeMotorOn);
+//            telemetry.update();
+//            if (isIntakeMotorOn){
+//                intakeMotor.setPower(-0.5);
+//            } else{
+//                intakeMotor.setPower(0);
+//            }
         }
-    }
-    public void processTelemetry(){
-        //add telemetry messages here
-        telemetry.addData("resetTimer: ",  resetTimer.milliseconds());
-        telemetry.addLine("---------------------------------");
-
-
-        telemetry.update();
     }
 }
