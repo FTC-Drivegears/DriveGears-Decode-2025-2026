22f1cf88fd39cfa3ef5a967f6308d2853958b6eb
Kayla0129 <kaylatongzhao@gmail.com>
2025-10-15 18:14:57 -0400

    my changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SampleAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SampleAutoOpMode.java
@@ -1,12 +1,17 @@
 package org.firstinspires.ftc.teamcode.opmodes.tests;
+import android.util.Size;
+
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Hardware;
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
-
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 
 @Autonomous (name = "Sample Auto")
@@ -14,47 +19,67 @@ public class SampleAutoOpMode extends LinearOpMode {
     private MecanumCommand mecanumCommand;
     private DcMotorEx motorIntake;
     enum AUTO_STATE {
-        FIRST_POSITION,
+        SCAN_OBELISK,
         BALL_PICKUP,
+
+        //SORTER
         FINISH
 
     }
 
+//    enum OBELISK_SORTER {
+//        GPP,
+//        PGP,
+//        PPG
+//
+//    }
+
     @Override
     public void runOpMode() throws InterruptedException {
-        // create Hardware using hardwareMap
         Hardware hw = Hardware.getInstance(hardwareMap);
         mecanumCommand = new MecanumCommand(hw);
         motorIntake = hardwareMap.get(DcMotorEx.class, "externalIntake");
         motorIntake.setDirection(DcMotorEx.Direction.FORWARD);
         motorIntake.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
-        AUTO_STATE autoState = AUTO_STATE.FIRST_POSITION;
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .setCameraResolution(new Size(640, 480))
+                .build();
+
+        waitForStart();
+
+        AUTO_STATE autoState = AUTO_STATE.SCAN_OBELISK;
         waitForStart();
         while (opModeIsActive()) {
             mecanumCommand.motorProcess();
             mecanumCommand.processOdometry();
 
-            //processPinPoint();
-
             switch (autoState) {
-                case FIRST_POSITION:
-                    mecanumCommand.moveToPos(30, 0, 0);
-                    motorIntake.setPower(0);  // Turn on intake while moving
+                case SCAN_OBELISK:
+                    if (tagProcessor.getDetections().size() > 0) {
+                        AprilTagDetection tag = tagProcessor.getDetections().get(0);
+                        sleep(3000);
+                    }
 
                     if (mecanumCommand.positionNotReachedYet()) {
                         autoState = AUTO_STATE.BALL_PICKUP;
-                        motorIntake.setPower(0);
                     }
                     break;
+
                 case BALL_PICKUP:
                     if (mecanumCommand.moveToPos(30, 0, 0)) {
                         autoState = AUTO_STATE.FINISH;
-                        motorIntake.setPower(0.5);  // Reverse intake to eject
+                        motorIntake.setPower(0.5);
                         sleep(3000);
-                        motorIntake.setPower(0);     // Then stop
+                        motorIntake.setPower(0);
                     }
                     break;
+
                 case FINISH:
                     motorIntake.setPower(0);  // Ensure intake is stopped
                     stopRobot();
