52859a121467c03163f00b4ad54959304f8e6d6a
shark <ck.barenthin@gmail.com>
2025-10-18 15:28:10 -0400

    added artifacts

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Artifact.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class Artifact {
+    private String colour;
+    private double position;
+
+    public Artifact(String colour, double position) {
+        this.colour = colour;
+        this.position = position;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
+
+    public double getPosition() {
+        return this.position;
+    }
+
+}
