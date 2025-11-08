62c6da9de0823ac2dd248511a0844e7889c4778b
Kayla0129 <kaylatongzhao@gmail.com>
2025-11-08 17:07:30 -0500

    obelisk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/Obelisk_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/Obelisk_Auto.java
@@ -4,7 +4,6 @@ import android.util.Size;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.Hardware;
 import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
@@ -12,25 +11,12 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
-@Autonomous (name = "AutoVision")
-public class Obelisk_Auto extends LinearOpMode {
-    private MecanumCommand mecanumCommand;
-
-    private ElapsedTime resetTimer;
-
-    enum AUTO_STATE {
-        SCAN_OBELISK,
-        PICKUP,
-        FINISH
-    }
+@Autonomous
+public class Obelisk_Auto extends LinearOpMode{
 
     @Override
     public void runOpMode() throws InterruptedException {
         AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
-                .setDrawAxes(true)
-                .setDrawCubeProjection(true)
-                .setDrawTagID(true)
-                .setDrawTagOutline(true)
                 .build();
 
         VisionPortal visionPortal = new VisionPortal.Builder()
@@ -39,54 +25,15 @@ public class Obelisk_Auto extends LinearOpMode {
                 .setCameraResolution(new Size(640, 480))
                 .build();
 
-        Hardware hw = Hardware.getInstance(hardwareMap);
-        mecanumCommand = new MecanumCommand(hw);
-        resetTimer = new ElapsedTime();
-
-        AUTO_STATE autoState = AUTO_STATE.SCAN_OBELISK;
         waitForStart();
-        while (opModeIsActive()) {
-            mecanumCommand.motorProcess();
-            mecanumCommand.processOdometry();
 
-            processTelemetry();
+        while (!isStopRequested() && opModeIsActive()) {
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
 
-            switch (autoState) {
-                case SCAN_OBELISK:
-                    if (mecanumCommand.moveToPos(0, 0,0)) {
-                        if (tagProcessor.getDetections().size() > 0) {
-                            AprilTagDetection tag = tagProcessor.getDetections().get(-1);
-                            telemetry.addData("ID", tag.id);
-                        }
-                    }
-                    telemetry.update();
-                    if (mecanumCommand.positionNotReachedYet()) {
-                        autoState = AUTO_STATE.FINISH;
-                    }
-                    break;
-                case PICKUP:
-//                    if (mecanumCommand.moveToPos(30, -20, 0)) {
-//                        autoState = AUTO_STATE.FINISH;
-//                    }
-//                    break;
-                case FINISH:
-                    stopRobot();
-                    break;
+                telemetry.addData("ID", tag.id);
             }
+            telemetry.update();
         }
-
-    }
-    public void processTelemetry(){
-        telemetry.addData("resetTimer: ",  resetTimer.milliseconds());
-        telemetry.addLine("---------------------------------");
-        telemetry.addData("X", mecanumCommand.getX());
-        telemetry.addData("Y", mecanumCommand.getY());
-        telemetry.addData("Theta", mecanumCommand.getOdoHeading());
-        telemetry.update();
-    }
-    private void stopRobot() {
-        mecanumCommand.moveGlobalPartialPinPoint(0, 0, 0);
     }
 }
-
-
