b7b717d3da5c8a6179db226fc9cc51064c8bacd4
Abhinav Prashant Boddapati <abhinav.boddapati.work@gmail.com>
2025-11-10 17:19:47 -0500

    Made some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/NewAutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/NewAutoOpMode.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmodes.tests;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Hardware;
@@ -9,8 +10,8 @@ import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
 
 
 
-@Autonomous (name = "Sample Auto")
-public class SampleAutoOpMode extends LinearOpMode {
+@Autonomous (name = "New Auto")
+public class NewAutoOpMode extends LinearOpMode {
     private MecanumCommand mecanumCommand;
     private int stage1 = 0;
 
@@ -46,86 +47,93 @@ public class SampleAutoOpMode extends LinearOpMode {
 
     private static double hoodPos = 1.0;
 
+    private static DcMotor shooter;
+    private static Servo pusher;
+    private static Servo hood;
+    private static Servo sorter;
+    private static DcMotor intake;
 
 
-    static boolean push(Hardware hw) throws InterruptedException {
+
+    static boolean push() {
         if(!isPusherUp){
-            hw.pusher.setPosition(PUSHER_UP);
+            pusher.setPosition(PUSHER_UP);
             pusherTimer.reset();
             isPusherUp = true;
         }
         if(isPusherUp && pusherTimer.milliseconds() >= 500){
-            hw.pusher.setPosition(PUSHER_DOWN);
+            pusher.setPosition(PUSHER_DOWN);
             isPusherUp = false;
         }
         return isPusherUp;
     }
 
-    static void sort(Hardware hw, int sorterPosition){
+    static void sort(int sorterPosition){
+        push();
         sorterTimer.reset();
-        if (sorterPosition == 0) {
-            hw.sorter.setPosition(0);
-        }
-        else if (sorterPosition == 1) {
-            hw.sorter.setPosition(0.43);
-        }
-        else if (sorterPosition == 2) {
-            hw.sorter.setPosition(0.875);
-        }
-    }
-
-
-    static void shoot(Hardware hw, boolean isOn){
-        if(isOn){
-            hw.shooter.setPower(1.0);
-        }
-        else{
-            hw.shooter.setPower(0.0);
+        if(sorterTimer.milliseconds() > 1000) {
+            if (sorterPosition == 0) {
+                sorter.setPosition(0);
+            } else if (sorterPosition == 1) {
+                sorter.setPosition(0.43);
+            } else if (sorterPosition == 2) {
+                sorter.setPosition(0.875);
+            }
         }
     }
-
-    static void unload(Hardware hw, int firstpos, int secondpos, int thirdpos) throws InterruptedException{
-        int num = 0;
-        shoot(hw, true);
-        switch (num){
-            case 0:
-                sort(hw, firstpos);
-                num++;
-                break;
-            case 1:
-                if(push(hw)){
-                    num++;
-                }
-                break;
-            case 2:
-                sort(hw, secondpos);
-                num++;
-                break;
-            case 3:
-                if(push(hw)){
-                    num++;
-                }
-                break;
-            case 4:
-                sort(hw, thirdpos);
-                break;
-            case 5:
-                if (push(hw)){
-                    num++;
-                }
-                break;
-        }
-        shoot(hw, false);
-    }
-
-    static void intake(Hardware hw, boolean isOn){
-        if(isOn){
-            hw.intake.setPower(0.8);
-        }
-        else{
-            hw.intake.setPower(0.0);
-        }
-    }
+//
+//
+//    static void shoot(Hardware hw, boolean isOn){
+//        if(isOn){
+//            shooter.setPower(0.8);
+//        }
+//        else{
+//            shooter.setPower(0.0);
+//        }
+//    }
+//
+//    static void unload(Hardware hw, int firstpos, int secondpos, int thirdpos) throws InterruptedException{
+//        int num = 0;
+//        shoot(hw, true);
+//        switch (num){
+//            case 0:
+//                sort(hw, firstpos);
+//                num++;
+//                break;
+//            case 1:
+//                if(push(hw)){
+//                    num++;
+//                }
+//                break;
+//            case 2:
+//                sort(hw, secondpos);
+//                num++;
+//                break;
+//            case 3:
+//                if(push(hw)){
+//                    num++;
+//                }
+//                break;
+//            case 4:
+//                sort(hw, thirdpos);
+//                break;
+//            case 5:
+//                if (push(hw)){
+//                    num++;
+//                }
+//                break;
+//        }
+//        shoot(hw, false);
+//    }
+//
+//    static void intake(Hardware hw, boolean isOn){
+//        if(isOn){
+//            intake.setPower(0.8);
+//        }
+//        else{
+//            intake.setPower(0.0);
+//        }
+//    }
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -134,9 +142,15 @@ public class SampleAutoOpMode extends LinearOpMode {
         mecanumCommand = new MecanumCommand(hw);
         resetTimer = new ElapsedTime();
 
-        hw.sorter.setPosition(initialPos);
-        hw.pusher.setPosition(PUSHER_DOWN);
-        hw.hood.setPosition(hoodPos);
+        shooter = hw.shooter;
+        pusher = hw.pusher;
+        sorter = hw.sorter;
+        hood = hw.hood;
+        intake = hw.intake;
+
+        sorter.setPosition(initialPos);
+        pusher.setPosition(PUSHER_DOWN);
+        hood.setPosition(hoodPos);
 
 
         AUTO_STATE autoState = AUTO_STATE.INITIAL_POSITION;
@@ -147,7 +161,8 @@ public class SampleAutoOpMode extends LinearOpMode {
             mecanumCommand.processOdometry();
 
             processTelemetry();
-            unload(hw, 0, 1, 2);
+
+            mecanumCommand.moveToPos(0, 0, 0.28447);
 
 //            switch (autoState) {
 //                case INITIAL_POSITION:
@@ -159,19 +174,29 @@ public class SampleAutoOpMode extends LinearOpMode {
 //                    mecanumCommand.moveToPos(0, 0, 0.28447);
 //                    if (mecanumCommand.isPositionReached()) {
 //                        autoState = AUTO_STATE.FIRST_SHOT;
+//
 //                    }
+//                    break;
 //                case FIRST_SHOT:
+//                    shooter.setPower(0.8);
 //                    switch(pattern){
 //                        case GPP_1:
-//                            unload(hw, 0, 1, 2);
+//                            sort(0);
+//                            sort(1);
+//                            sort(2);
 //                            break;
 //                        case PGP_2:
-//                            unload(hw, 1, 2, 0);
+//                            sort(1);
+//                            sort(2);
+//                            sort(0);
 //                            break;
 //                        case PPG_3:
-//                            unload(hw, 2, 0, 1);
+//                            sort(2);
+//                            sort(0);
+//                            sort(1);
 //                            break;
 //                    }
+//                    shooter.setPower(0.0);
 //                    autoState = AUTO_STATE.COLLECTION;
 //                    break;
 //                case COLLECTION:
