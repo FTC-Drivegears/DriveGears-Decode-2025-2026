b77d2a06a3151327814ac638e22e6a0df5ac9607
Kayla0129 <kaylatongzhao@gmail.com>
2026-03-18 11:05:04 -0400

    changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/coloursensor/ColourSensorSubsystem.java
@@ -0,0 +1,106 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests.coloursensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Hardware;
+import org.firstinspires.ftc.teamcode.util.Artifact;
+
+import java.util.ArrayList;
+
+public class ColourSensorSubsystem {
+
+    private Hardware hw;
+    private ColorSensor colourSensor1;
+    private ColorSensor colourSensor2;
+    private Servo sorter;
+    private Servo light;
+
+    private boolean detectedColor = false;
+    private ArrayList<Artifact> sorterList = new ArrayList<>();
+
+    private int red, green, blue, alpha;
+    private int red2, green2, blue2, alpha2;
+
+    public ColourSensorSubsystem(HardwareMap hardwareMap, Hardware hw) {
+        this.hw = hw;
+        this.colourSensor1 = hardwareMap.get(ColorSensor.class, "colour1");
+        this.colourSensor2 = hardwareMap.get(ColorSensor.class, "colour2");
+
+        this.sorter = hw.sorter;
+        this.light = hw.light;
+
+        colourSensor1.enableLed(true);
+        colourSensor2.enableLed(true);
+    }
+
+    public void update(boolean intakeOn) {
+        red = colourSensor1.red();
+        green = colourSensor1.green();
+        blue = colourSensor1.blue();
+        alpha = colourSensor1.alpha();
+
+        red2 = colourSensor2.red();
+        green2 = colourSensor2.green();
+        blue2 = colourSensor2.blue();
+        alpha2 = colourSensor2.alpha();
+
+        if (!intakeOn) return;
+        if (sorterList.size() == 3) return;
+
+        // PURPLE
+//        if (!detectedColor && blue > red + 30 || blue > green + 30 && alpha < 700) {
+//            detectedColor = true;
+//            sorterList.add(new Artifact("Purple", sorter.getPosition()));
+//            light.setPosition(0.7);
+//            turnSorter();
+
+        if (blue > red + 30 && blue > green + 30 && alpha < 700 || blue2 > red2 + 30 && blue2 > green2 + 30 && alpha2 < 700) {
+            if (!detectedColor) {
+                detectedColor = true;
+                sorterList.add(new Artifact("Purple", sorter.getPosition()));
+                light.setPosition(0.7);
+                turnSorter();
+            }
+
+            //GREEN
+//        } else if(!detectedColor && green - blue > 20 && alpha > 600 || green2 - blue2 > 20 && alpha2 > 600) {
+//        detectedColor = true;
+//        sorterList.add(new Artifact("Green", sorter.getPosition()));
+//        light.setPosition(0.5);
+//        turnSorter();
+//    }
+
+            // detect GREEN
+        } else if (green - blue > 20 && alpha > 600 || green2 - blue2 > 20 && alpha2 > 600) {
+            if (!detectedColor) {
+                detectedColor = true;
+                sorterList.add(new Artifact("Green", sorter.getPosition()));
+                light.setPosition(0.5);
+                turnSorter();
+            }
+        }
+
+        boolean ballPresent = alpha > 50 || alpha2 > 50;
+        if (!ballPresent) {
+            detectedColor = false;
+            light.setPosition(0);
+        }
+    }
+
+    private void turnSorter() {
+        double pos = Math.min(sorterList.size() * 0.43, 1.0);
+        sorter.setPosition(pos);
+    }
+
+    public int getRed() { return red; }
+    public int getGreen() { return green; }
+    public int getBlue() { return blue; }
+    public int getAlpha() { return alpha; }
+    public int getCount() { return sorterList.size(); }
+    public int getRed2() { return red2; }
+    public int getGreen2() { return green2; }
+    public int getBlue2() { return blue2; }
+    public int getAlpha2() { return alpha2; }
+}
+
