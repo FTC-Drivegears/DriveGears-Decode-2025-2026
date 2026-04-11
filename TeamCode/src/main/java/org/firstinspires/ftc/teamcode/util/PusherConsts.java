d43e15f1c9f2a40845f5e3cc28553c474ce48b97
UploadWizRobotics <upload@wizrobotics.com>
2026-04-11 17:37:39 -0400

    Changed values for pusher, not fully certain aboout bottom values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PusherConsts.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PusherConsts.java
@@ -7,6 +7,7 @@ public class PusherConsts{
     public static final double PUSHER_DOWN_POSITION_L = 0.1;
     public static final double PUSHER_UP_POSITION_L = 0.35;
 // 0.4 it will snap
+
 // 0.2 it will also snap
 // 0.85 is too low
 // this is all with pusher.setPosition(1); as down
